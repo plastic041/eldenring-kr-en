7ea91a3d5f3a44717dfaf87401db67b399c9aee6
plastic041@ <plastic041@gmail.com>
2022-03-24 14:44:55 +0900

    ResultCard | 검색어 입력해주세요 안내

--- src/App.tsx
+++ src/App.tsx
@@ -93,6 +93,57 @@ const Header = () => {
   );
 };
 
+const ResultCard = ({ query, ko, en, category }: Dict & { query: string }) => {
+  return (
+    <Box component="dl" key={en} sx={{ margin: 0 }}>
+      <Paper
+        withBorder
+        shadow="xs"
+        p="md"
+        sx={(theme) => ({
+          display: "flex",
+          flexDirection: "column",
+
+          "& div": {
+            display: "flex",
+
+            "& dt, & dd": {
+              display: "inline",
+            },
+
+            "& dt": {
+              width: "3rem",
+              marginRight: "1rem",
+              color: theme.colors.gray[7],
+            },
+
+            "& dd": {
+              margin: 0,
+              color: theme.colors.gray[9],
+              whiteSpace: "pre-wrap",
+            },
+          },
+        })}
+      >
+        <div>
+          <dt>한국어</dt>
+          <dd>
+            <Highlight highlight={query}>{ko}</Highlight>
+          </dd>
+        </div>
+        <div>
+          <dt>영어</dt>
+          <dd>{en}</dd>
+        </div>
+        <div>
+          <dt>분류</dt>
+          <dd>{category}</dd>
+        </div>
+      </Paper>
+    </Box>
+  );
+};
+
 const Body = () => {
   const [query, setQuery] = useState("");
   const [categories, setCategories] = useState<string[]>(["의상", "무기"]);
@@ -109,6 +160,8 @@ const Body = () => {
         return isCategoryInclude && isKoInclude;
       });
       setResult(find);
+    } else {
+      setResult([]);
     }
   };
 
@@ -153,73 +206,33 @@ const Body = () => {
       </Group>
       <ScrollArea>
         {results?.length ? (
+          // 검색 결과가 있을 때
           <Box
             className="search-results"
-            sx={{
+            sx={(theme) => ({
               display: "flex",
               flexDirection: "column",
-              gap: "1rem",
-            }}
+              gap: theme.spacing.md,
+              marginBottom: theme.spacing.md,
+            })}
           >
             {results.map((result) => (
-              <Box component="dl" key={result.en} sx={{ margin: 0 }}>
-                <Paper
-                  withBorder
-                  shadow="xs"
-                  p="md"
-                  sx={(theme) => ({
-                    display: "flex",
-                    flexDirection: "column",
-
-                    "& div": {
-                      display: "flex",
-
-                      "& dt, & dd": {
-                        display: "inline",
-                      },
-
-                      "& dt": {
-                        width: "3rem",
-                        marginRight: "1rem",
-                        color: theme.colors.gray[7],
-                      },
-
-                      "& dd": {
-                        margin: 0,
-                        color: theme.colors.gray[9],
-                        whiteSpace: "pre-wrap",
-                      },
-                    },
-                  })}
-                >
-                  <div>
-                    <dt>한국어</dt>
-                    <dd>
-                      <Highlight highlight={query}>{result.ko}</Highlight>
-                    </dd>
-                  </div>
-                  <div>
-                    <dt>영어</dt>
-                    <dd>{result.en}</dd>
-                  </div>
-                  <div>
-                    <dt>분류</dt>
-                    <dd>{result.category}</dd>
-                  </div>
-                </Paper>
-              </Box>
+              <ResultCard key={result.en} query={query} {...result} />
             ))}
           </Box>
         ) : categories.length === 0 ? (
+          // 검색 결과가 없을 때
           <Text size="sm" color="red">
-            카테고리를 하나 이상 선택해주세요
+            카테고리를 하나 이상 선택해주세요.
+          </Text>
+        ) : query ? (
+          <Text size="md" align="center">
+            <Mark>{query}</Mark>에 대한 검색 결과가 없습니다.
           </Text>
         ) : (
-          query && (
-            <Text size="md" align="center">
-              <Mark>{query}</Mark>에 대한 검색 결과가 없습니다.
-            </Text>
-          )
+          <Text size="md" align="center">
+            검색어를 입력해주세요.
+          </Text>
         )}
       </ScrollArea>
     </>
