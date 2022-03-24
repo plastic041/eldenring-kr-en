c7f356aac4b36a74136b327a769c51776e2ceba2
plastic041@ <plastic041@gmail.com>
2022-03-24 16:05:04 +0900

    복사 기능

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -1,56 +1,89 @@
-import { Box, Highlight, Paper } from "@mantine/core";
+import {
+  ActionIcon,
+  Box,
+  Highlight,
+  Paper,
+  Text,
+  Tooltip,
+} from "@mantine/core";
+import { CheckIcon, ClipboardCopyIcon } from "../Icons";
 
 import { Dict } from "../App";
 import { useClipboard } from "@mantine/hooks";
 
 const ResultCard = ({ query, ko, en, category }: Dict & { query: string }) => {
+  const clipboard = useClipboard({
+    timeout: 1000,
+  });
+
   return (
-    <Box component="dl" key={en} sx={{ margin: 0 }}>
-      <Paper
-        withBorder
-        shadow="xs"
-        p="md"
-        sx={(theme) => ({
+    <Paper
+      withBorder
+      component="dl"
+      shadow="xs"
+      p="md"
+      sx={(theme) => ({
+        display: "flex",
+        flexDirection: "column",
+        margin: 0,
+
+        "& div": {
           display: "flex",
-          flexDirection: "column",
 
-          "& div": {
-            display: "flex",
-
-            "& dt, & dd": {
-              display: "inline",
-            },
-
-            "& dt": {
-              width: "3rem",
-              marginRight: "1rem",
-              color: theme.colors.gray[7],
-            },
-
-            "& dd": {
-              margin: 0,
-              color: theme.colors.gray[9],
-              whiteSpace: "pre-wrap",
-            },
+          "& dt, & dd": {
+            display: "inline",
           },
-        })}
-      >
-        <div>
-          <dt>한국어</dt>
-          <dd>
-            <Highlight highlight={query}>{ko}</Highlight>
-          </dd>
-        </div>
-        <div>
-          <dt>영어</dt>
-          <dd>{en}</dd>
-        </div>
-        <div>
-          <dt>분류</dt>
-          <dd>{category}</dd>
-        </div>
-      </Paper>
-    </Box>
+
+          "& dt": {
+            width: "3rem",
+            marginRight: "1rem",
+            color: theme.colors.gray[7],
+            flexShrink: 0,
+          },
+
+          "& dd": {
+            margin: 0,
+            color: theme.colors.gray[9],
+            whiteSpace: "pre-wrap",
+          },
+        },
+      })}
+    >
+      <div>
+        <dt>한국어</dt>
+        <dd>
+          <Highlight highlight={query}>{ko}</Highlight>
+        </dd>
+      </div>
+      <div>
+        <dt>영어</dt>
+        <Box
+          sx={(theme) => ({
+            display: "flex",
+            alignItems: "center",
+          })}
+        >
+          <Text>{en}</Text>
+          <Tooltip
+            label={clipboard.copied ? `복사됨` : `복사`}
+            transition="pop"
+          >
+            <ActionIcon
+              variant="hover"
+              onClick={() => clipboard.copy(en)}
+              title={`${en} 복사`}
+              color={clipboard.copied ? "green" : "gray"}
+            >
+              {clipboard.copied ? <CheckIcon /> : <ClipboardCopyIcon />}
+            </ActionIcon>
+          </Tooltip>
+        </Box>
+      </div>
+      <div>
+        <dt>분류</dt>
+        <dd>{category}</dd>
+      </div>
+    </Paper>
   );
 };
 export default ResultCard;
