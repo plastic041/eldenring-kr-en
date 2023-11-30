e39e3d9ca023439b2636cb53193dc65105ea537c
plastic041 <plastic041@gmail.com>
2023-11-30 10:53:08 +0900

    remove ColorSchemeScript

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,12 +1,9 @@
-import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 
 import type { ReactNode } from "react";
 
 export const Providers = ({ children }: { children: ReactNode }) => {
   return (
-    <>
-      <ColorSchemeScript defaultColorScheme="auto" />
-      <MantineProvider defaultColorScheme="auto">{children}</MantineProvider>
-    </>
+    <MantineProvider defaultColorScheme="auto">{children}</MantineProvider>
   );
 };
